3638ae32cd3ff8c4fbc49ab699ba7dfecdc5566f
Arturo Filio Villa <arturfil@hotmail.com>
2019-04-20 00:05:50 -0400

    added a member-details component

---
+++ src/app/members/member-detail/member-detail.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { User } from '../../_models/user';
+import { UserService } from '../../_services/user.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+
+@Component({
+  selector: 'app-member-detail',
+  templateUrl: './member-detail.component.html',
+  styleUrls: ['./member-detail.component.css']
+})
+export class MemberDetailComponent implements OnInit {
+  // creating property
+  user: User;
+
+  constructor(
+    private userService: UserService,
+    private alertify: AlertifyService,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    this.loadUser();
+  }
+
+  // members/:id
+  loadUser() {
+    this.userService.getUser(+this.route.snapshot.params['id']).subscribe((user: User) => {
+      this.user = user;
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
+
+}
